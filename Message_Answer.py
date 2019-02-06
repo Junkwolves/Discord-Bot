f5c7654d3a8f1fa1154388087f1922258602ccc8
Junkwolves <justajunk2069@gmail.com>
2019-02-06 18:40:42 +0200

    Create Message_Answer.py
    Message & Answer Ref.

---
+++ Message_Answer.py
@@ -0,0 +1,13 @@
+import discord
+import const
+
+from discord.ext import commands 
+from discord.ext.commands import Bot
+
+Bot = commands.Bot(command_prefix = '>')
+
+@Bot.command(pass_context = True)
+async def User_Message(ctx):
+    await Bot.say("Bot_Answer")
+
+Bot.run(token)
